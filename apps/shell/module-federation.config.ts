1e614a53ba08777cad08641701be887e59d08c93
KhallilB <khallilbailey@gmail.com>
2025-06-26 11:19:38 -0700

    re init

--- apps/shell/module-federation.config.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @nx/enforce-module-boundaries */
-import { shared } from '../../module-federation.base.config';
-
-export default {
-  name: 'shell',
-  remotes: [],
-  shared,
-};
--- module-federation.base.config.ts
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function shared(pkg: string) {
-  // Don't share this package; each app can load its own version.
-  //   if (pkg === '@acme/ui') return false;
-}
