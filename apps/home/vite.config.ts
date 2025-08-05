0f82e4a60e5718c3ba48afc5bd459e598e64c91f
KhallilB <khallilbailey@gmail.com>
2025-08-05 00:06:51 -0700

    chore: change  page name

--- apps/home/vite.config.ts
+++ apps/home/vite.config.ts
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 3001
+  },
   resolve: {
     alias: {
       '@styles/*': '../../libs/styles/src/*'
